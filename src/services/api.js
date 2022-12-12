1d41cd7784cec8d8157ec51b6fc5a2c9add62a49
Mireia Maldonado Parra <mirmalparra@gmail.com>
2022-12-12 17:53:19 +0100

    Sort characters by name

--- src/services/api.js
+++ src/services/api.js
@@ -13,7 +13,15 @@ const getDataFromApi = () => {
                 status: character.status
             }
         })
-        return cleanData;
+        return cleanData.sort(function (a, b) {
+            if (a.name > b.name) {
+              return 1;
+            }
+            if (a.name < b.name) {
+              return -1;
+            }
+            return 0;
+          });
     })
 };
 
