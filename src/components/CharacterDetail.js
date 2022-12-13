d8018f47f63301fdf69d11729aa34251fc93950c
Mireia Maldonado Parra <mirmalparra@gmail.com>
2022-12-13 15:50:29 +0100

    Add icon for status

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -3,6 +3,9 @@ import arrow from '../images/arrow.png';
 import alien from "../images/alien.png";
 import human from "../images/human.png";
 import NotFound from './NotFound';
+import dead from "../images/dead.png";
+import alive from "../images/alive.png";
+import unknown from "../images/unknown.png";
 const CharacterDetail = (props) => {
     const params = useParams();
     const characterFound = props.findCharacter(params.characterId);
@@ -10,12 +13,23 @@ const CharacterDetail = (props) => {
     if (characterFound === undefined) {
         return <NotFound />
       } else {
+
     const returnSpecies = () => {
        if (characterFound.species === "Alien"){
         return alien;
        } else {
         return human;
        }
+    }
+
+    const returnStatus = () => {
+        if (characterFound.status === "Dead"){
+            return dead;
+           } else if (characterFound.status === "Alive"){
+            return alive;
+           } else {
+            return unknown;
+           }
     }
     return (
         <section className="detail">
@@ -26,7 +40,7 @@ const CharacterDetail = (props) => {
             <img className="list__icon"src={`${returnSpecies()}`} alt={characterFound.species}/>
             <p className="detail__p">{characterFound.planet}</p>
             <p>{characterFound.episodes}</p>
-            <p>{characterFound.status}</p>
+            <img className="list__icon"src={`${returnStatus()}`} alt={characterFound.status}/>
             </div>
             <Link to='/'><img className="detail__arrow" src={arrow} alt="arrow back"/></Link>
         </article>
