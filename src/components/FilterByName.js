d0599d23b33e4d9687ba7437ffab63e33a104adf
Mireia Maldonado Parra <mirmalparra@gmail.com>
2022-12-14 12:59:37 +0100

    Make header and form responsive

--- src/components/FilterByName.js
+++ src/components/FilterByName.js
@@ -4,8 +4,8 @@ const FilterByName = (props) => {
     };
     return (
        <>
-           <label htmlFor="name" className="form__label">Busca un personaje</label>
-            <input className="form__input" type="text" name="name" id="name" placeholder="Rick" value={props.filterByName} onChange={handleChange} />
+           <label htmlFor="name" className="form__label">Busca un personaje </label>
+            <input className="form__input" type="text" name="name" id="name" placeholder="Ej: Rick" value={props.filterByName} onChange={handleChange} />
             </>
     );
 };
